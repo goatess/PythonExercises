52dd36d70fb53838421b3056ab5c44d39ab644c0
goatess <107187023+goatess@users.noreply.github.com>
2022-08-02 01:37:10 +0200

    sort list added

--- estructurasControl/InvertirNumeros.py
+++ estructurasControl/InvertirNumeros.py
@@ -1,7 +1,9 @@
 
 nums = []
-num = 100
-while num > 0:
+num = 1
+while num <= 100:
     nums.append(num)
-    num -= 1
-print(*nums)
+    num += 1
+    
+sortedNums = sorted(nums, reverse = True)
+print(*sortedNums)
