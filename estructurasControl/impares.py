b53d981236963010f04c78c6ccdbe4b0b72e1cf2
goatess <107187023+goatess@users.noreply.github.com>
2022-08-02 01:15:06 +0200

    print list

--- estructurasControl/impares.py
+++ estructurasControl/impares.py
@@ -1,6 +1,7 @@
 
 nums = [0,1,2,3,4,5,6,7,8,9]
 for num in nums:
-    isOdd = num % 2 != 0
-    if isOdd:
-        print(num)
+    isEven = num % 2 == 0
+    if isEven:
+        nums.remove(num)
+print (*nums)
