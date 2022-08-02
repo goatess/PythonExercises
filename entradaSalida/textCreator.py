3512d2d5ac054e2b70e332d123fc14b6d3326c79
goatess <107187023+goatess@users.noreply.github.com>
2022-08-02 22:15:24 +0200

    save object to file and load object from file

--- entradaSalida/textCreator.py
+++ entradaSalida/textCreator.py
@@ -3,12 +3,13 @@ class TextBuilder:
     def read():
         file = open('entradaSalida/note.txt', 'r')
         text = file.readlines()
-        print(text)
         file.close()
+        print(text)
 
     def write(text):
         file = open('entradaSalida/note.txt', 'w')
         file.write(text)
+        print(text)
         file.close()
 
 TextBuilder.write("Este es el nuevo texto de la nota")
--- entradaSalida/vehicleBuilder.py
+++ entradaSalida/vehicleBuilder.py
@@ -1,4 +1,13 @@
+import pickle 
 import vehicle
 
 car = vehicle.Car("red")
+file = open('entradaSalida/car.bin', 'wb')
+pickle.dump(car, file)
+file.close()
+print(car.color)
+
+file = open('entradaSalida/car.bin', 'rb')
+car = pickle.load(file)
+file.close()
 print(car.color)
