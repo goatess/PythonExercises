622a44bc9f70f834c67d7fd05716554bf9e3885e
goatess <107187023+goatess@users.noreply.github.com>
2022-08-06 20:04:48 +0200

    filter method exercise

--- bibliotecaEstandar/ej2/filteredAddition.py
+++ bibliotecaEstandar/ej2/filteredAddition.py
@@ -2,15 +2,19 @@
 # pasada por parámetro con filter y realizará una suma de todos estos 
 # elementos obtenidos mediante reduce.
 
-
-def getOdds(nums):
-    for num in nums:
-        isEven = num % 2 == 0
-        if isEven:
-            nums.remove(num)
-    print (*nums)
-    
-def addition(nums):
-    for num in nums:
-        sum  += num
+def check(number):
+    if number % 2 != 0:
+        return True
+    else: 
+        return False
         
+def addition(numbersList):
+    sum = 0
+    for number in numbersList:
+        sum += number
+    return sum
+      
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+odds = list(filter(check, numbers))
+print(odds)
+print(addition(odds))
